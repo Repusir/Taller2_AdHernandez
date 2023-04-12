a19dc06e9ad68dc8c66a5809296560423ab73535
Adian Hernandez <repusoir@gmail.com>
2023-04-11 21:14:23 -0600

    [HotFix]: Changing compilation folder path

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 sudo npm install
 sudo tsc
-sass src/assets/styles/sass/style.scss:src/assets/styles/css/style.css #--watch --style compressed
+sass src/assets/styles/sass/style.scss:src/assets/styles/style.css #--watch --style compressed
